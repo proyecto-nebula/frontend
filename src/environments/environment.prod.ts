c906456b7d2d5623d26715dce6b40871842e95e1
Angel <piscos@gmail.com>
2026-05-25 21:09:17 +0200

    Fix url api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-production-a381.up.railway.app/api/v1/',
+  apiUrl: 'https://proyecto-nebula.up.railway.app/api/v1/',
 };
